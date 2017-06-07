a05974154837f7ad93768639d29b7be987b5731a
Kai-Hsiang Hsu <r04922156@csie.ntu.edu.tw>
2017-06-07 13:30:08 +0800

    use concurrent.futures

--- experiment.py
+++ experiment.py
@@ -6,12 +6,58 @@ import re
 import pprint
 import operator
 import os
+import concurrent.futures
 
 import Controller as MyController
 import Device as MyDevice
 
 import SimpleLTLPolicy as MySimpleLTLPolicy
 import InvariantPolicy as MyInvariantPolicy
+
+def buildRandomLTLSetting(database, available_rules, number_of_rules):
+    # initial controller
+    controller = MyController.Controller(database)
+
+    # add devices
+    for channel_name, channel_definition in controller.getFeasibleDevices():
+        name = re.sub('[^A-Za-z0-9_]+', '', channel_name).lower()
+        device = MyDevice.Device(channel_name, channel_definition, name)
+
+        possible_values_of_variables = device.getPossibleValuesOfVariables()
+        state = dict((variable_name, random.choice(tuple(possible_values))) for variable_name, possible_values in possible_values_of_variables.items())
+        device.setState(state)
+
+        controller.addDevice(device)
+
+    # add rules
+    current_rules = random.sample(available_rules, number_of_rules)
+    for count, rule in enumerate(current_rules, start=1):
+        trigger_channel_name, trigger_name, action_channel_name, action_name = rule
+
+        rule_name = 'RULE{}'.format(count)
+        trigger_inputs = controller.getFeasibleInputsForTrigger(trigger_channel_name, trigger_name)
+        action_inputs = controller.getFeasibleInputsForAction(action_channel_name, action_name)
+        controller.addRule(rule_name, trigger_channel_name, trigger_name, trigger_inputs, action_channel_name, action_name, action_inputs)
+
+    # random choose vulnerables
+    device_name = random.choice(tuple(controller.devices))
+    controller.addVulnerableDevice(device_name)
+
+    # random build invariant policy
+    device_name, variable_name = random.choice(tuple(controller.device_variables))
+    device = controller.getDevice(device_name)
+    variable = device.getVariable(variable_name)
+    value = random.choice(tuple(variable.getPossibleValues()))
+    policy = MySimpleLTLPolicy.LTLPolicy('{0}.{1} != {2} | {0}.{1} = {2}'.format(device_name, variable_name, value))
+
+    return {'controller': controller, 'policy': policy}
+
+def checkModel(setting):
+    controller = setting['controller']
+    policy = setting['policy']
+    grouping = setting['grouping']
+    pruning = setting['pruning']
+    return controller.check(policy, grouping=grouping, pruning=pruning)
 
 if __name__ == '__main__':
     # show information
@@ -36,58 +82,31 @@ if __name__ == '__main__':
 
     number_of_trials = 100
     for number_of_rules in range(25, 1001, 25):
-        original = (0, 0, 0, 0)
-        optimized = (0, 0, 0, 0)
-        original_overtime = 0
-        optimized_overtime = 0
-        for trial in range(number_of_trials):
-            # initial controller
-            controller = MyController.Controller(database)
+        print('{0:>6} {1:>15} {2:>15} {3:>15} {4:>15}'.format(number_of_rules, 'grouping', 'pruning', 'parsing', 'checking'))
+        settings = [buildRandomLTLSetting(database, available_rules, number_of_rules) for i in range(number_of_trials)]
 
-            # add devices
-            for channel_name, channel_definition in controller.getFeasibleDevices():
-                name = re.sub('[^A-Za-z0-9_]+', '', channel_name).lower()
-                device = MyDevice.Device(channel_name, channel_definition, name)
+        for grouping, pruning in [(False, False), (True, True)]:
+            grouping_times = list()
+            pruning_times = list()
+            parsing_times = list()
+            checking_times = list()
 
-                possible_values_of_variables = device.getPossibleValuesOfVariables()
-                state = dict((variable_name, random.choice(tuple(possible_values))) for variable_name, possible_values in possible_values_of_variables.items())
-                device.setState(state)
+            for setting in settings:
+                setting['grouping'] = grouping
+                setting['pruning'] = pruning
 
-                controller.addDevice(device)
+            with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+                for result, grouping_time, pruning_time, parsing_time, checking_time in executor.map(checkModel, settings):
+                    grouping_times.append(grouping_time)
+                    pruning_times.append(pruning_time)
+                    parsing_times.append(parsing_time)
+                    checking_times.append(checking_time)
 
-            # add rules
-            current_rules = random.sample(available_rules, number_of_rules)
-            for count, rule in enumerate(current_rules, start=1):
-                trigger_channel_name, trigger_name, action_channel_name, action_name = rule
+            grouping_time = sum(grouping_times) / number_of_trials
+            pruning_time = sum(pruning_times) / number_of_trials
+            parsing_time = sum(parsing_times) / number_of_trials
+            checking_time = sum(checking_times) / number_of_trials
+            overtime = sum(checking_time > 3600 for checking_time in checking_times)
 
-                rule_name = 'RULE{}'.format(count)
-                trigger_inputs = controller.getFeasibleInputsForTrigger(trigger_channel_name, trigger_name)
-                action_inputs = controller.getFeasibleInputsForAction(action_channel_name, action_name)
-                controller.addRule(rule_name, trigger_channel_name, trigger_name, trigger_inputs, action_channel_name, action_name, action_inputs)
-
-            # random choose vulnerable
-            device_name = random.choice(tuple(controller.devices))
-            controller.addVulnerableDevice(device_name)
-
-            # random build invariant policy
-            device_name, variable_name = random.choice(tuple(controller.device_variables))
-            device = controller.getDevice(device_name)
-            variable = device.getVariable(variable_name)
-            value = random.choice(tuple(variable.getPossibleValues()))
-            policy = MySimpleLTLPolicy.LTLPolicy('{0}.{1} != {2} | {0}.{1} = {2}'.format(device_name, variable_name, value))
-
-            # print(count)
-            result, *time = controller.check(policy, grouping=False, pruning=False)
-            original = tuple(map(operator.add, original, time))
-            original_overtime += 1 if result == None else 0
-
-            result, *time = controller.check(policy, custom=False, grouping=True, pruning=True)
-            optimized = tuple(map(operator.add, optimized, time))
-            optimized_overtime += 1 if result == None else 0
-
-        original = tuple(v / number_of_trials for v in original)
-        optimized = tuple(v / number_of_trials for v in optimized)
-        print('{0:>6} {1:>15} {2:>15} {3:>15} {4:>15}'.format('number', 'grouping', 'pruning', 'parsing', 'checking'))
-        print('{0:^6} {1:15f} {2:15f} {3:15f} {4:15f} ({5})'.format(number_of_rules, *original, original_overtime))
-        print('{0:^6} {1:15f} {2:15f} {3:15f} {4:15f} ({5})'.format('', *optimized, optimized_overtime))
+            print('{0:^6} {1:15f} {2:15f} {3:15f} {4:15f} ({5})'.format('', grouping_time, pruning_time, parsing_time, checking_time, overtime))
 
